600e0a84d507ec68233b06082fdd2dbb3aabf44f
mariusrueve <marius.rueve@live.de>
2023-10-31 16:10:26 +0100

    Add MCS calculation

--- src/SingleAligner.cpp
+++ src/SingleAligner.cpp
@@ -1,6 +1,11 @@
 #include "SingleAligner.hpp"
 
 #include <GraphMol/MolAlign/AlignMolecules.h>
+#include <GraphMol/FMCS/FMCS.h>
+#include <RDGeneral/export.h>
+#include <vector>
+#include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/Substruct/SubstructMatch.h>
 
 namespace ciw {
 
@@ -15,7 +20,16 @@ namespace ciw {
     }
 
     void SingleAligner::algin_molecules(RDKit::ROMol mol_a, RDKit::ROMol mol_b) {
-        //RDKit::MolAlign::getAlignmentTransform(mol_a, mol_b);
+        // zip mol_a and mol_b into a vector
+        std::vector<RDKit::ROMOL_SPTR> mols;
+        mols.push_back(RDKit::ROMOL_SPTR(new RDKit::ROMol(mol_a)));
+        mols.push_back(RDKit::ROMOL_SPTR(new RDKit::ROMol(mol_b)));
+
+        // find the MCS
+        RDKit::MCSResult res = RDKit::findMCS(mols);
+
+        // print the MCS
+        std::cout << "MCS: " << res.SmartsString << std::endl;
     }
 
 } // ciw
