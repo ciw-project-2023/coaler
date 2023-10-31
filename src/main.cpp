0e41747b07e5f46b56ea1abfdc19aab622d027b2
Niklas Paulicks <nPaulicks@gmail.com>
2023-10-31 14:16:22 +0100

    added missing link dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ int main(int argc, char *argv[]) {
     RDKit::RWMol* mol_a = RDKit::SmilesToMol("CCCO");
     RDKit::RWMol* mol_b = RDKit::SmilesToMol("CCCN");
 
+    std::cout << mol_a->getNumAtoms() << std::endl;
+
     ciw::SingleAligner single_aligner;
 
     //single_aligner.algin_molecules(mol_a->, mol_b);
