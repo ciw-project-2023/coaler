d65ba824c5908306bc3a1a5eabe13bb596e632b3
Niklas Paulicks <paulicks@justtrack.io>
2023-10-31 11:36:01 +0100

    conan file

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class alignerRecipe(ConanFile):
+    name = "aligner"
+    version = "0.0.1"
+    package_type = "application"
+
+    # Optional metadata
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of aligner package here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def requirements(self):
+        self.requires("rdkit/0.0.1@nikplx/local")
+        self.requires("mytestlib/0.0.1")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    
+
+    
