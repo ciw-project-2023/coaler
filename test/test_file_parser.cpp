93a444c6faa61ccbf3d0a5df080f150f3c0ebf06
Niklas Paulicks <paulicks@justtrack.io>
2023-11-09 15:32:16 +0100

    added test data absolute path from projet root

--- test/test_file_parser.cpp
+++ test/test_file_parser.cpp
@@ -6,6 +6,6 @@
 #include <filesystem>
 
 TEST_CASE("File Parser","[parser]") {
-    auto assay_targets_1806504 = FileParserSmiles::parse("./data/AID_1806504.smi");
+    auto assay_targets_1806504 = FileParserSmiles::parse("test/data/AID_1806504.smi");
     REQUIRE(!assay_targets_1806504.empty());
 }
