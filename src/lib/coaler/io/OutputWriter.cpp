7a5d30a0dcd66b8a3ad1f8e571aac12809592d45
HokageM <m.trzaska663@gmail.com>
2023-11-29 15:42:15 +0100

    adjust OutputWriter

--- src/lib/coaler/io/OutputWriter.cpp
+++ src/lib/coaler/io/OutputWriter.cpp
@@ -4,25 +4,33 @@
 
 #include "OutputWriter.hpp"
 
-#include <GraphMol/SmilesParse/SmartsWrite.h>
+#include <GraphMol/SmilesParse/SmilesWrite.h>
+#include <GraphMol/RWMol.h>
 #include <spdlog/spdlog.h>
 
 namespace coaler::io {
-    void OutputWriter::add_aligned_mols(AlignedMolPair& aligned_mol_pair) {
-        aligned_mols_.emplace_back(aligned_mol_pair);
-    }
-
-    void OutputWriter::save_molecules_w_scores_in_file(const std::string& file_path) {
-        // TODO: define here output format
-    }
-
-    void OutputWriter::print_in_log_molecules_w_scores() {
-        spdlog::info("Start printing results.");
-        for (auto mol_pair : aligned_mols_) {
-            spdlog::info("Molecule {}:{} and molecule {}:{} are aligned with score: {}", mol_pair.id_mol_a,
-                         MolToSmarts(mol_pair.mol_a), mol_pair.id_mol_b, MolToSmarts(mol_pair.mol_b),
-                         mol_pair.align_score);
+    void OutputWriter::save_molecules_w_scores_in_file(MultiAlignerResult result, const std::string& file_path) {
+        for (auto ligand_id_conformer_pair : result.poseIDsByLigandID) {
+            LigandID ligand_id = std::get<0>(ligand_id_conformer_pair);
+            PoseID poseId = std::get<1>(ligand_id_conformer_pair);
+            RDKit::RWMol* ligand_w_ptr = result.ligands.at(ligand_id);
+            const RDKit::ROMol ligand_o = *ligand_w_ptr;
         }
+
+        // TODO: write sdf file
+    }
+
+    void OutputWriter::print_multi_aligner_result(MultiAlignerResult result) {
+        spdlog::info("Start printing results.");
+        for (auto ligand_id_conformer_pair : result.poseIDsByLigandID) {
+            LigandID ligand_id = std::get<0>(ligand_id_conformer_pair);
+            PoseID poseId = std::get<1>(ligand_id_conformer_pair);
+            RDKit::RWMol* ligand_w_ptr = result.ligands.at(ligand_id);
+            const RDKit::ROMol ligand_o = *ligand_w_ptr;
+
+            spdlog::info("Molecule Smiles:{} ID:{} Conformere: {}", RDKit::MolToSmiles(ligand_o), ligand_id, poseId);
+        }
+        spdlog::info("Multi Alginement Score: {}", result.score);
         spdlog::info("End of results. :)");
     }
 }  // namespace coaler::io
