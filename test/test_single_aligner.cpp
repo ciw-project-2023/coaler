bc9496c3dde1fb7aae5d00fd274ce558bd279c6a
mariusrueve <marius.rueve@live.de>
2023-12-07 14:29:39 +0100

    Refactor test_single_aligner.cpp for Tanimoto and Kabsch alignment

--- test/test_single_aligner.cpp
+++ test/test_single_aligner.cpp
@@ -8,13 +8,51 @@
 #include <coaler/singlealign/SingleAligner.hpp>
 
 TEST_CASE("Singlealign", "[singlealign]") {
-    SECTION("Align two similar molecules") {
-        auto input = coaler::io::FileParser::parse("test/data/two_mols.sdf");
-        RDKit::RWMol *mol = input.at(0);
-        coaler::SingleAligner singleAligner;
-        //  double similarity = singleAligner.calculate_tanimoto_shape_similarity(*mol, *mol);
-        double similarity = 1;
-        CHECK(similarity == 1);
+    auto input = coaler::io::FileParser::parse("test/data/two_mols.sdf");
+    RDKit::RWMol *mol_a = input.at(0);
+    RDKit::RWMol *mol_b = input.at(1);
+    coaler::SingleAligner singleAligner;
+
+    SECTION("Tanimoto Similarity"){
+        SECTION("Two similar molecules") {
+            double similarity = singleAligner.calculate_tanimoto_shape_simularity(*mol_a, *mol_a, -1, -1);
+            CHECK(similarity == 1);
+        }
+
+        SECTION("Two different molecules") {
+            double similarity = singleAligner.calculate_tanimoto_shape_simularity(*mol_a, *mol_b, -1, -1);
+            // Fix for floating point precision
+            similarity = static_cast<unsigned int>(similarity * 100000) / 100000.0;
+            CHECK(similarity == 0.01809);
+        }
     }
 
+    SECTION("Kabsch Alignment"){
+        SECTION("Two similar molecules") {
+            std::vector<RDKit::ROMOL_SPTR> mols;
+            mols.emplace_back(boost::make_shared<RDKit::ROMol>(*mol_a));
+            mols.emplace_back(boost::make_shared<RDKit::ROMol>(*mol_a));
+
+            RDKit::MCSResult res = RDKit::findMCS(mols);
+            RDKit::ROMOL_SPTR core_structure = res.QueryMol;
+
+            double score = singleAligner.align_molecules_kabsch(*mol_a, *mol_a, -1, -1, *core_structure);
+            score = static_cast<unsigned int>(score * 100000) / 100000.0;
+            CHECK(score == 0);
+        }
+
+        SECTION("Two different molecules") {
+            std::vector<RDKit::ROMOL_SPTR> mols;
+            mols.emplace_back(boost::make_shared<RDKit::ROMol>(*mol_a));
+            mols.emplace_back(boost::make_shared<RDKit::ROMol>(*mol_b));
+
+            RDKit::MCSResult res = RDKit::findMCS(mols);
+            RDKit::ROMOL_SPTR core_structure = res.QueryMol;
+
+            double similarity = singleAligner.align_molecules_kabsch(*mol_a, *mol_b, -1, -1, *core_structure);
+            // Fix for floating point precision
+            similarity = static_cast<unsigned int>(similarity * 100000) / 100000.0;
+            CHECK(similarity == 1.34124);
+        }
+    }
 }
