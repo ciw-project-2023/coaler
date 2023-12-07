8c3f157efe95550bc373a53192252bb57bf8e0b6
Christoph <christoph.noack@studium.uni-hamburg.de>
2023-12-08 00:36:08 +0100

    moved tests to renamed file

--- test/test_conformer_generator.cpp
+++ test/test_conformer_generator.cpp
@@ -1,9 +1,13 @@
+
 #include <GraphMol/DistGeomHelpers/Embedder.h>
 #include <GraphMol/SmilesParse/SmilesParse.h>
 #include <GraphMol/Substruct/SubstructMatch.h>
 
+#include <boost/range/combine.hpp>
+
 #include "catch2/catch.hpp"
 #include "coaler/embedder/ConformerEmbedder.hpp"
+#include "coaler/embedder/SubstructureAnalyzer.hpp"
 
 using namespace coaler::embedder;
 
@@ -26,7 +30,9 @@ namespace {
     }
 }  // namespace
 
-TEST_CASE("test_shared_core", "[embedder]") {
+/*----------------------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("test_shared_core", "[conformer_generator_tester]") {
     RDKit::ROMol mol1 = *RDKit::SmilesToMol("c1ccccc1CCCO");
     RDKit::ROMol mol2 = *RDKit::SmilesToMol("c1c(CC)cc(CC)cc1CC");
     RDKit::ROMol core = *RDKit::SmilesToMol("c1ccccc1");
@@ -60,4 +66,38 @@ TEST_CASE("test_shared_core", "[embedder]") {
             }
         }
     }
+}
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+void check_distribution(unsigned nofMatches, unsigned maxConfs, const std::vector<unsigned>& expected_dist) {
+    std::vector<unsigned> dist = ConformerEmbedder::distributeApproxEvenly(nofMatches, maxConfs);
+    REQUIRE(dist.size() == expected_dist.size());
+    for (const auto& itertuple : boost::combine(dist, expected_dist)) {
+        CHECK(itertuple.get<0>() == itertuple.get<1>());
+    }
+}
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("validate_distribute_evenly", "[conformer_generator_tester]") {
+    check_distribution(3, 7, {3, 2, 2});
+    check_distribution(2, 7, {4, 3});
+    check_distribution(5, 10, {2, 2, 2, 2, 2});
+    check_distribution(6, 20, {4, 4, 3, 3, 3, 3});
+}
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("test_ring_symmetry_determination", "[conformer_generator_tester]") {
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("c1ccccc1")) == 6);
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("C1CCCCC1")) == 6);
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("N1NNNNN1")) == 6);
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("C1NCCNC1")) == 2);
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("C1NCNCN1")) == 3);
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("C1NCNCNCN1")) == 4);
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("C1NSOCNSO1")) == 2);
+
+    // rings of odd size are not rotation symmetric
+    CHECK(SubstructureAnalyzer::getNumberOfRingRotations(*RDKit::SmilesToMol("C1CCCCCC1")) == 1);
 }
