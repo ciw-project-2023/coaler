b871ba9fbc7a8e5961fe2d03a55e854240c3d788
Christoph <christoph.noack@studium.uni-hamburg.de>
2023-12-22 22:59:27 +0100

    added conformer log that saves the generated conformers of each mol in a seperate file.
    	- also changed arg -f to -i for sanity reasons

--- src/lib/coaler/io/OutputWriter.hpp
+++ src/lib/coaler/io/OutputWriter.hpp
@@ -21,7 +21,14 @@ namespace coaler::io {
          * Writes the aligned molecules in an output file.
          * @param file_path
          */
-        static void writeSDF(const std::string &file_path, const coaler::multialign::MultiAlignerResult &result);
+        static void writeSDF(const std::string& file_path, const coaler::multialign::MultiAlignerResult& result);
+
+        /**
+         * Writes all conformers of a molecule to a sdf file.
+         * @param folder_path A path to the folder store the files in
+         * @param mol A molecule whose conformers are to be written to an .sdf file
+         */
+        static void writeConformersToSDF(const std::string& folder_path, const std::vector<RDKit::ROMOL_SPTR>& mols);
     };
 
 }  // namespace coaler::io
