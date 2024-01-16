adea547e76232ba407c553b6e9d9a751d7852957
HokageM <m.trzaska663@gmail.com>
2024-01-16 18:48:10 +0100

    remove more generated files

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/projects/coaler/test
-# Build directory: /home/mike/projects/coaler/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyTest "/home/mike/projects/coaler/test/Test")
-set_tests_properties(MyTest PROPERTIES  _BACKTRACE_TRIPLES "/home/mike/projects/coaler/test/CMakeLists.txt;24;add_test;/home/mike/projects/coaler/test/CMakeLists.txt;0;")
