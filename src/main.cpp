07644f207515482fb7c06800b4f9042a3d5dca2e
Christoph <christoph.noack@studium.uni-hamburg.de>
2024-01-21 13:36:32 +0100

    adapt default parameters

--- src/main.cpp
+++ src/main.cpp
@@ -67,9 +67,8 @@ std::optional<ProgrammOptions> parse_args(int argc, char* argv[]) {
         "divide,d", opts::value<bool>(&parsedOptions.divide_conformers_by_matches)->default_value(false),
         "divides the number of conformers by the number of times the core is matched")(
         "confs-log", opts::value<std::string>(&parsedOptions.conformer_log_path)->default_value("none"))(
-        "optimizer-coarse-threshold",
-        opts::value<double>(&parsedOptions.coarse_optimization_threshold)
-            ->default_value(multialign::constants::COARSE_OPTIMIZATION_THRESHOLD))(
+        "optimizer-coarse-threshold", opts::value<double>(&parsedOptions.coarse_optimization_threshold)
+                                          ->default_value(multialign::constants::COARSE_OPTIMIZATION_THRESHOLD))(
         "optimizer-fine-threshold", opts::value<double>(&parsedOptions.fine_optimization_threshold)
                                         ->default_value(multialign::constants::FINE_OPTIMIZATION_THRESHOLD))(
         "optimizer-step-limit", opts::value<int>(&parsedOptions.optimizer_step_limit)
